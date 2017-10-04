468718ce52240d4fdf727979eeaa766fb91bff0d
martin byrne <marb61a@gmail.com>
2017-10-04 16:34:26 +0000

    updated  webpack severside rendering config file

--- app/server.js
+++ app/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import template from './template';
 
 const app = express();
+app.use('/assets', express.static('assets'));
 
 function renderClient(req, res){
     res.send(template);
