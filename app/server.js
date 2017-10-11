5e54c890ededa3a24e765ed2215f7f99ecc7bf05
martin byrne <marb61a@gmail.com>
2017-10-11 16:13:51 +0000

    added invoice model and updated server js

--- app/server.js
+++ app/server.js
@@ -1,10 +1,28 @@
-import express from 'express';
+import express from 'express'
+import kx from './server/connection';
 import render from './serverRender';
 
 const app = express();
 app.use('/assets', express.static('assets'));
 
-
 app.use(render);
+
+kx.on('query', data => console.log('data'));
+
+const invoices = kx.from('invoices')
+        .select('total', 'email', 'username')
+        .innerJoin('users', 'users.id', 'invoices.user_id')
+        .then(rows => {
+            debugger;
+            return rows;
+        });
+
+const users = kx.from('users')
+        .select('total', 'email', 'username')
+        .innerJoin('invoices', 'invoices.user_id', 'users.id')
+        .then(rows => {
+            debugger;
+            return rows;
+        });
 
 app.listen(8080);
---
+++ app/server/models/invoice.js
@@ -0,0 +1,10 @@
+import bookshelf from '../connection';
+import Customer from './Customer';
+
+export default bookshelf.Model.extend({
+  tableName: 'invoices',
+
+  customer: function () {
+    return this.belongsTo(Customer);
+  }
+})
