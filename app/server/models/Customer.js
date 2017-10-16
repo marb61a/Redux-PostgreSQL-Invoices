bbeb09395670a160ba4deb0264a23bcbfe1f708a
martin byrne <marb61a@gmail.com>
2017-10-16 12:09:48 +0000

    updated customer model

--- app/server/models/Customer.js
+++ app/server/models/Customer.js
@@ -6,6 +6,32 @@ export default bookshelf.Model.extend({
     tableName: 'customers',
     
     initialize: function(){
-        
+        this.on('saving', (model) => {
+            const errors = {};
+            const values = model.toJSON();
+            
+            const unique = model.where({
+                email: values.email,
+                user_id: values.user_id
+            }).fetch.then((customer) => {
+                if(customer && customer.id !== values.id){
+                    errors.email = 'Must be Unique';
+                }
+            });
+            
+            const format = new Promise((resolve) => {
+                if(!email.validate(values.email)){
+                    errors.email = 'Not Valid';
+                }
+                
+                resolve();
+            });
+            
+            return Promise.all([format, unique]).then(() => {
+                if(Object.keys(errors).length){
+                    throw new ValidationException(errors);
+                }
+            });
+        });
     }
 })
