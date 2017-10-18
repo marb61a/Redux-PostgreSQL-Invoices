19eb5ca5c675e57d18d00eeabaa1fde84383e9ca
martin byrne <marb61a@gmail.com>
2017-10-18 16:32:19 +0000

    added customer actions.spec file

--- app/universal/customers/actions.spec.js
+++ app/universal/customers/actions.spec.js
@@ -11,5 +11,42 @@ const mockStore = configureStore([thunk]);
 const mockAdapter = new MockAdapter(adapter);
 
 describe('customer actions', () => {
+    afterEach(() => {
+        mockAdapter.reset();
+    });
     
+    describe('list()', () => {
+        const data = {
+            totalCount: 0,
+            results: []
+        };
+        
+        beforeEach(() => {
+            mockAdapter.onGet('/customers').reply(200, data);
+        });
+        
+        it('dispatches resultsUpdated', () => {
+            const store = mockStore();
+            
+            return store.dispatch(actions.list()).then(() => {
+                expect(store.getActions()).toInclude(actions.resultsUpdated(data));
+            });
+        });
+    });
+    
+    describe('create()', () => {
+        context('on validation failure', () => {
+            beforeEach(() => {
+                mockAdapter.onPost('/customers').reply(422, { errors: {} });
+            });
+        });
+        
+        it('throws a submission error', () => {
+            const store = mockStore();
+            
+            return store.dispatch(actions.create({})).catch((err) => {
+                expect(err).toBeA(SubmissionError);
+            });
+        });
+    });
 })
