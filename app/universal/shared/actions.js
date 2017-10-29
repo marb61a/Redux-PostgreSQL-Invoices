95bea2b323f611c47bc5acac06690e3319c727e6
martin byrne <marb61a@gmail.com>
2017-10-29 12:01:44 +0000

    added shared actions file

---
+++ app/universal/shared/actions.js
@@ -0,0 +1,24 @@
+import actionType, * as actionTypes from './actionTypes';
+
+export default function actions(listId){
+    const t = actionType(listId);
+    
+    return {
+        resultsUpdated: ({ results, totalCount}) => ({
+            type: t(actionTypes.RESULTS_UPDATED),
+            results,
+            totalCount
+        }),
+        previous: () => {
+            type: t(actionTypes.PREV);
+        },
+        next: () => {
+            type: t(actionTypes.NEXT);
+        },
+        setPage: (page, pageSize = undefined) => {
+            type: t(actionTypes.SET_PAGE),
+            page,
+            pageSize;
+        }
+    };
+}
