ac17f0ed29ce59a0cf09ff63cd5807867e3bde75
martin byrne <marb61a@gmail.com>
2017-11-01 16:35:23 +0000

    updated server render jsx file

--- app/serverRender.jsx
+++ app/serverRender.jsx
@@ -13,18 +13,60 @@ import * as invoicesService from './server/services/invoices';
 
 function preloadedRoutes(store, req){
     const loadPage = (listId, service) => {
-        const { resultsUpdated, setPage } = actions(listId);    
-    };    
+        const { resultsUpdated, setPage } = actions(listId);  
+        
+        const {page = 1, pageSize = store.getState()[listId].pageSize} = req.query;
+        store.dispatch(setPage(page, pageSize));
+        
+        return service.list({page, pageSize}, req.currentUser) 
+            .then(page => {
+                store.dispatch(resultsUpdated(page));
+            });
+    };
+    
+    return[{
+        path: '/customers',
+        load: () => loadPage('cusomers', customersService)
+    }, {
+        path: '/invoices',
+        load: () => loadPage('invoices', invoicesService)
+    }];
+}
+
+function loader(req, store){
+    const route = preloadedRoutes(store, req).find(r = matchPath(req.path, r));
+    
+    if(!route || !req.currentUser){
+        return () => Promise.resolve();
+    }
+    
+    return route.load;
 }
 
 export default function render(req, res){
-    const store = createStore(reducer);
-    store.dispatch(updateColor);
+    const store = createStore(true);
     
-    const html = renderToString(
-        <Provider>
-            <RGB />
-        </Provider>);
-        
-    res.send(template(html));
+    if(req.currentUser){
+        store.dispatch(setUser(req.currentUser));
+    }
+    
+    const context = {};
+    const dataLoader = loader(req, store);
+    
+    dataLoader
+        .then(() => {
+            const html = renderToString(
+                <Provider store={store}>
+                    <StaticRouter location={req.url} context={context}>
+                        <App />
+                    </StaticRouter>
+                </Provider>);
+            
+            if(context.url){
+                res.redirect(301, context.url);
+            } else {
+                const status = context.status || 200;
+                res.status(status).send(template(html, store.getState()));
+            }
+        });
 }
