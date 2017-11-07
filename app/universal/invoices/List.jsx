fa9494e28ff5eeecb855ed7b2c5122e4b8bad04b
martin byrne <marb61a@gmail.com>
2017-11-07 12:26:29 +0000

    added List invoice and rgbreduce files

---
+++ app/universal/invoices/List.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Table from './Table';
+import { Flipper } from '../shared/pagination';
+
+export default function List(){
+    return(
+        <div className="pure-g">
+            <div className="pure-1">
+                <h1>Manage Invoices</h1>
+            </div>
+            <div className="pure-u-1">
+                <Flipper listId="invoices" />
+            </div>
+            <div className="pure-u-1">
+                <Table />
+            </div>        
+        </div>    
+    );
+}
